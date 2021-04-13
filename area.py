47eca2489980747656b988f4150358f26508a616
shanmuga-priyan271 <82431638+shanmuga-priyan271@users.noreply.github.com>
2021-04-13 10:47:00 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,3 +1,7 @@
 r=int(input("Enter the radius of the circle:")
 area=22/7*r*r
 print("The area of circle with radius r is:",area)
+//program to print extension//
+filename=input("Input the file name:")
+extns=filename.split(".")
+print("The extension of the file is:",+repr(extns[-1])
